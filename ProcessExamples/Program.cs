f908f9150911db7cbc367bdcc0294183db138242
Robin <robin.kaiser@haevg-rz.de>
2021-03-18 13:48:33 +0100

    added examples for processes

--- ProcessExamples/Program.cs
+++ ProcessExamples/Program.cs
@@ -8,31 +8,54 @@ namespace ProcessExamples
     {
         static void Main(string[] args)
         {
+            //Console.WriteLine(args[2]);
+
             // 1. basic example
-            Process.Start("mspaint.exe");
+            //Process.Start("mspaint");
 
             // 2. using an overload to supply arguments.
             // This opens the file test.txt in notepad, if it exists
-            Process.Start(@"C:\Program Files\Google\Chrome\Application\chrome.exe", "https://www.google.com/");
-            
+            //Process.Start(@"C:\Program Files\Google\Chrome\Application\chrome.exe", 
+            //    "https://www.google.com/");
+
             // 3. using ProcessStartInfo
             // This loads the google start page in chrome.
-            ProcessStartInfo psi = new ProcessStartInfo()
-            {
-                FileName = @"notepad.exe",
-                Arguments = @"C:\Local\test.txt",
-                CreateNoWindow = true,
-                UseShellExecute = false,
-            };
-            Process process = new Process()
-            {
-                StartInfo = psi,
-            };
-            process.Start();
-            Thread.Sleep(5000);
-            process.Kill();
+            //ProcessStartInfo psi = new ProcessStartInfo()
+            //{
+            //    FileName = @"notepad.exe",
+            //    Arguments = @"C:\Local\test.txt",
+            //    CreateNoWindow = true,
+            //    //UseShellExecute = false,
+            //};
 
-            Console.WriteLine("Hello World!");
+            //Process process = new Process()
+            //{
+            //    StartInfo = psi,
+            //};
+
+            //process.Start();
+            //Thread.Sleep(5000);
+            //process.Kill();
+            Console.WriteLine("hallo");
+
+            // 4. Redirect Standard output
+            var psi = new ProcessStartInfo()
+            {
+                FileName = @"C:\Local\hello.exe",
+                UseShellExecute = true,
+                CreateNoWindow = false,
+                //RedirectStandardOutput = true,
+            };
+            var p = new Process() { StartInfo = psi };
+            p.Start();
+            Thread.Sleep(3000);
+            Console.WriteLine("Hey");
+            //string output = p.StandardOutput.ReadToEnd();
+            //Console.WriteLine($"output: {output}");
+
+
+
+            Console.ReadKey();
         }
     }
 }
---
+++ ProcessWpfExample/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.Diagnostics;
+using System.Threading;
+using System.Windows;
+
+namespace ProcessWpfExample
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            
+        }
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            
+            var psi = new ProcessStartInfo()
+            {
+                //FileName = @"C:\Local\hello.exe",
+                FileName = @"C:\Program Files (x86)\Notepad++\notepad++.exe",
+                Arguments = @"C:\Local\test.txt",
+                UseShellExecute = true,
+                CreateNoWindow = true,
+                //RedirectStandardOutput = true,
+            };
+            var p = new Process() { StartInfo = psi };
+            p.Start();
+            //Thread.Sleep(3000);
+
+            //string output = p.StandardOutput.ReadToEnd();
+            //Console.WriteLine($"output: {output}");
+        }
+    }
+}
